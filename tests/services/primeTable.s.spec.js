d1b1d4de5d56da520b13e955b9653da1b883e092
Shaoyun Li <Shaoyun Li>
2017-03-17 14:43:11 +0000

    Add a test which fails the current implementation

--- tests/services/primeTable.s.spec.js
+++ tests/services/primeTable.s.spec.js
@@ -1,31 +1,48 @@
-describe('The Prime Table Service', function(){
+describe('The Prime Table Service', function () {
 
     var thePrimeTableService, mockValidationService, mockPrimeGeneratorService, createService;
 
     beforeEach(module('primeTables'));
 
     beforeEach(inject(function ($injector) {
-        
+
         mockValidationService = $injector.get('validationService');
         mockPrimeGeneratorService = $injector.get('primeGeneratorService');
-        thePrimeTableService = $injector.get('primeTableService');     
+        thePrimeTableService = $injector.get('primeTableService');
     }))
 
-    it('should have primeTableService be defined', function(){
+    it('should have primeTableService be defined', function () {
         expect(mockValidationService).toBeDefined();
         expect(mockPrimeGeneratorService).toBeDefined();
         expect(thePrimeTableService).toBeDefined();
     })
 
-    it('should return null if the provide size is invalid', function(){
-        
+    it('should return null if the provide size is invalid', function () {
+
         // Arrange        
         spyOn(mockValidationService, ['validate']).and.returnValue(false);
 
         // Act
-        var primes = thePrimeTableService.getPrimeTable(0);
+        var primeTable = thePrimeTableService.getPrimeTable(0);
 
         // Assert
-        expect(primes).toEqual(null);
-    })    
+        expect(primeTable).toEqual(null);
+    })
+
+    it('should return an array with 2 columns and 2 rows if the provide size is 1', function () {
+
+        // Arrange        
+        spyOn(mockValidationService, ['validate']).and.returnValue(true);
+        var table = [
+            [0, 2],
+            [2, 4]
+        ];
+        spyOn(mockPrimeGeneratorService, ['getPrimes']).and.returnValue([2]);
+
+        // Act
+        var primeTable = thePrimeTableService.getPrimeTable(2);
+
+        // Assert
+        expect(primeTable).toEqual(table);
+    })
 })
